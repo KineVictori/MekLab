e56dacec26f84c031ced7cde919f172c51bf3397
KineVictori <kine.aurora@hotmail.com>
2025-03-25 10:47:32 +0100

    RViz funker

--- MiniProsjekt/src/qube_description/launch/view_qube.launch.py
+++ MiniProsjekt/src/qube_description/launch/view_qube.launch.py
@@ -2,7 +2,6 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
-import os
 
 def generate_launch_description():
     pkg_name = 'qube_description'
@@ -23,6 +22,12 @@ def generate_launch_description():
         }]
     )
 
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui'
+    )
+
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
@@ -33,5 +38,6 @@ def generate_launch_description():
 
     return LaunchDescription([
         robot_state_publisher_node,
-        rviz_node
+        rviz_node,
+        joint_state_publisher_node
     ])
