8c9c89cfd8421dcca41fbe523a1f7f40bd99c57f
KineVictori <kine.aurora@hotmail.com>
2025-03-25 11:58:01 +0100

    kommentarer

--- MiniProsjekt/src/qube_description/launch/view_qube.launch.py
+++ MiniProsjekt/src/qube_description/launch/view_qube.launch.py
@@ -3,12 +3,20 @@ from launch_ros.actions import Node
 from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
+# generate_launch_description function er 'main' funksjonen i launch fila.
+# Den returnerer et LaunchDescription-objekt som inneholder alle nodene som skal startes
 def generate_launch_description():
+
+    # pkg_name er navnet til pakka
     pkg_name = 'qube_description'
+    # pkg_share er pathen til share mappa til pakka
     pkg_share = FindPackageShare(package=pkg_name).find(pkg_name)
 
+    # default_rviz_config_path er pathen til rviz config fila
+    # RViz er et visualiseringsverktøy for ROS, og brukes til å visualisere roboter og sensorer
     default_rviz_config_path = PathJoinSubstitution([pkg_share, 'rviz/qube_config.rviz'])
 
+    # robot_state_publisher_node er brukt til å publishe robotens tilstand
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -22,12 +30,14 @@ def generate_launch_description():
         }]
     )
 
+    # joint_state_publisher_node er brukt til å publisere joint tilstander
     joint_state_publisher_node = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
         name='joint_state_publisher_gui'
     )
 
+    # rviz_node blir brukt for å visualisere roboten
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
@@ -36,6 +46,7 @@ def generate_launch_description():
         arguments=['-d', default_rviz_config_path],
     )
 
+    # Returner launch description
     return LaunchDescription([
         robot_state_publisher_node,
         rviz_node,
