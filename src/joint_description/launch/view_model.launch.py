ddc4abdca0e591cd6a60572ad387b6dc8df69b51
IslandRock1 <oysteinmb@gmail.com>
2025-03-27 12:10:55 +0100

    Lab3Oppg2DelA

--- src/joint_description/launch/view_model.launch.py
+++ src/joint_description/launch/view_model.launch.py
@@ -2,13 +2,12 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
+import xacro
 
 def generate_launch_description():
     pkg_share = get_package_share_directory('joint_description')
     urdf_file = os.path.join(pkg_share, 'urdf', 'joint_model.urdf')
-    
-    with open(urdf_file, 'r') as file:
-        robot_description_content = file.read()
+    robot_description_content = xacro.process_file(urdf_file).toxml()
 
     return LaunchDescription([
         Node(
@@ -16,7 +15,8 @@ def generate_launch_description():
             executable='rviz2',
             name='rviz2',
             output='screen',
-            parameters=[{'use_sim_time': True}]
+            parameters=[{'use_sim_time': True}],
+            arguments = ['d', [os.path.join(pkg_share, 'config', 'Config.rviz')]]
         ),
         Node(
             package='robot_state_publisher',
